ccfec66e1cfb56b3857de1c97909506777892cf3
Joshua Bullock (RIT Student) <jtb3607@ad.rit.edu>
2020-02-26 16:08:25 -0500

    added some files for collision

---
+++ Assets/Scripts/SpawnBridges.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBridges : MonoBehaviour
+{
+    public GameObject BridgePath;
+    public GameObject Player;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+       
+        BridgePath.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+    }
+
+}
